45411ef3bc5990963732f68124e0eee0bd16046c
doit-now <hoang.nguyenthe@gmail.com>
2022-06-15 11:58:38 +0700

    refactor the main code and green 4ever always - fix exception msg

--- src/com/giaolang/mathutil/core/MathUtil.java
+++ src/com/giaolang/mathutil/core/MathUtil.java
@@ -60,7 +60,7 @@ public class MathUtil {
      public static long getFactorial(int n) {
          
         if (n < 0 || n > 20)
-            throw new IllegalArgumentException("n must be between 0..20. Plz!");
+            throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
          
         if (n == 0 || n == 1)
             return 1; //kết thúc cuộc chơi sớm nếu nhận những đầu vào đặc biệt
